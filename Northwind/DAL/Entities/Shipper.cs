381f9430f64432090f246c742a2b26571804899b
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-09-05 19:20:59 +0300

    Entities: Shipper

--- Northwind/DAL/Entities/Shipper.cs
+++ Northwind/DAL/Entities/Shipper.cs
@@ -1,6 +1,26 @@
-﻿namespace Northwind.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind.DAL.Entities
 {
+    [Table("Shippers")]
     public class Shipper
     {
+        public Shipper()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ShipperID { get; set; }
+
+        [Required, MaxLength(40)]
+        public string CompanyName { get; set; }
+
+        [MaxLength(24)]
+        public string? Phone { get; set; }
+
+        // Navigation Properties
+        public virtual ICollection<Order> Orders { get; set; }
     }
 }

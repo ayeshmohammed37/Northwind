177ec8697163d73e356512d7bdf5f19e9d918613
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-09-05 16:42:46 +0300

    Entites: edit Product

--- Northwind/DAL/Entities/Category.cs
+++ Northwind/DAL/Entities/Category.cs
@@ -1,7 +1,19 @@
-﻿namespace Northwind.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind.DAL.Entities
 {
+    [Table("Categories")]
     public class Category
     {
-        
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryID { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+
+        // Navigation Properties
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
--- Northwind/DAL/Entities/Product.cs
+++ Northwind/DAL/Entities/Product.cs
@@ -6,22 +6,25 @@ namespace Northwind.DAL.Entities
     [Table("Products")]
     public class Product
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ProductID { get; set; }
-        [Required]
+        [Required, MaxLength(40)]
         public string ProductName { get; set; }
         [ForeignKey("Supplier")]
         public int? SupplierID { get; set; }
         [ForeignKey("Category")]
         public int? CategoryID { get; set; }
+        [MaxLength(20)]
         public string? QuantityPerUnit { get; set; }
+        [Column(TypeName ="money")]
         public double? UnitPrice { get; set; }
-        public int? UnitsInStock { get; set; }
-        public int? UnitsOnOrder { get; set; }
-        public int? ReorderLevel { get; set; }
+        public short? UnitsInStock { get; set; }
+        public short? UnitsOnOrder { get; set; }
+        public short? ReorderLevel { get; set; }
         public bool Discontinued { get; set; }
 
         // Navigation Properies
-        public virtual Supplier Supplier { get; set; }
-        public virtual Category Category { get; set; }
+        public virtual Supplier? Supplier { get; set; }
+        public virtual Category? Category { get; set; }
     }
 }

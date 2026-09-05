cd1f930259e3d655c9da69ccdb54824912f40c8e
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-09-05 17:13:41 +0300

    Entities: Suppliers

--- Northwind/DAL/Entities/Supplier.cs
+++ Northwind/DAL/Entities/Supplier.cs
@@ -1,7 +1,42 @@
-﻿namespace Northwind.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind.DAL.Entities
 {
+    [Table("Suppliers")]
     public class Supplier
     {
+        public Supplier()
+        {
+            Products = new HashSet<Product>();
+        }
 
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SupplierID { get; set; }
+        [Required, MaxLength(40)]
+        public string CompanyName { get; set; }
+        [MaxLength(30)]
+        public string? ContactName { get; set; }
+        [MaxLength(30)]
+        public string? ContactTitle { get; set; }
+        [MaxLength(60)]
+        public string? Address { get; set; }
+        [MaxLength(15)]
+        public string? City { get; set; }
+        [MaxLength(15)]
+        public string? Region { get; set; }
+        [MaxLength(10)]
+        public string? PostalCode { get; set; }
+        [MaxLength(15)]
+        public string? Country { get; set; }
+        [MaxLength(24)]
+        public string? Phone { get; set; }
+        [MaxLength(24)]
+        public string? Fax { get; set; }
+        [Column(TypeName ="ntext")]
+        public string? HomePage { get; set; }
+
+        // Navigation Properties
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
